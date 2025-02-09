9dd1728d276a211cd35a766b0b84123718b4ece8
mango6-ai <mango6k@gmail.com>
2025-02-09 22:48:29 +0900

    _오후10

--- push.bat
+++ push.bat
@@ -1,14 +1,22 @@
 @echo off
-echo Git 상태 확인:
+echo Git 상태 확인
 git status
 echo.
 
-echo 모든 변경 사항 추가:
+echo 모든 변경 사항 추가
 git add .
 echo.
 
-echo 커밋 메시지와 함께 커밋:
-set /p commitMessage="커밋 메시지를 입력하세요: "
+REM echo 커밋 메시지와 함께 커밋:
+REM set /p commitMessage="커밋 메시지를 입력하세요: "
+REM git commit -m "%commitMessage%"
+REM git commit -m "%commitMessage%"
+REM echo.
+
+echo 현재 시간을 커밋 메시지로 사용:
+for /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%a-%%b-%%c)
+for /f "tokens=1-2 delims=: " %%a in ('time /t') do (set mytime=%%a%%b)
+set commitMessage=%mydate%_%mytime%
 git commit -m "%commitMessage%"
 echo.
 
